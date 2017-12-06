a9f4311c09b3e5e1cbb9cacb9665313b1ce6104a
Yuyang Zhang <helloqiu95@gmail.com>
2017-12-07 00:35:36 +0800

    replace requests with grequests

--- mianliao.py
+++ mianliao.py
@@ -1,22 +1,19 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, request, render_template, jsonify
-from gevent import monkey
-import requests
+import grequests
 import json
 
-monkey.patch_all()
-
 app = Flask(__name__)
-
 
 @app.route('/api/login', methods=['GET', 'POST'])
 def index():
   if request.method == "POST":
     login_data = json.loads(request.get_data())
-    r = requests.get(
+    r = grequests.get(
       "http://prodv1.tjut.cc/api/login/",
       params={
         "account": login_data['username'],
         "password": login_data['password']
       })
+    r = grequests.map([r])[0]
     return jsonify(r.json())
